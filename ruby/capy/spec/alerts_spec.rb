f869f94cddaf5fbb2c658679b5b74d9cba018149
guivinilincoln <gui.vini.lincoln@gmail.com>
2020-07-07 18:19:02 -0300

    Alerts and Login

---
+++ ruby/capy/spec/alerts_spec.rb
@@ -0,0 +1,59 @@
+describe 'Alertas em JS', :alert do
+
+
+    before(:each) do
+
+        visit 'https://training-wheels-protocol.herokuapp.com/javascript_alerts'
+
+    end
+
+    it 'alerta' do
+        click_button 'Alerta'
+
+        msg = page.driver.browser.switch_to.alert.text # usado para informar para o capybara que vamos utilziar o navegador
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+        sleep 1        
+
+    end
+
+    it 'sim confirma' do
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.accept #usado para aceitar a mensagem
+        expect(page).to have_content 'Mensagem confirmada'
+
+    end
+
+    it 'não confirma' do
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+
+        page.driver.browser.switch_to.alert.dismiss #usado para cancelar a mensagem
+        expect(page).to have_content 'Mensagem não confirmada'
+
+    end
+
+    it 'Aceita prompt', :accept do
+
+        accept_prompt(with: 'Guilherme') do
+            click_button 'Prompt'
+            sleep 2
+        end
+        expect(page).to have_content 'Olá, Guilherme'
+        sleep 2
+    end
+
+    it 'Não aceita prompt', :n_aceita do
+
+        dismiss_prompt(with: 'Guilherme') do
+            click_button 'Prompt'
+            sleep 2
+        end
+        expect(page).to have_content 'Olá, null'
+        sleep 2
+    end
+
+end
---
+++ ruby/capy/spec/loginDinamico_spec.rb
@@ -0,0 +1,31 @@
+describe 'Login Campo dinamico', :dinamico do
+   
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/login2'
+    end
+
+    it 'com data de nascimento' do
+
+        fill_in 'username', with: 'stark' # usado para preencher campo input type por name (name: username)
+        fill_in 'passId', with: 'jarvis!' # usado para preencher campo input type por Id (id: passId)
+        #find('#passId').set '' essa forma tbm serve
+
+      
+        login_form = find('#login')
+
+        case login_form.text
+        when /Dia/
+            find('#day').set '29'
+        when /Mês/
+            find('#month').set '05'
+        when /Ano/
+            find('#year').set '1970'
+        end
+
+        click_button 'Login'
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+
+    end
+
+
+end
